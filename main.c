7477a6696ebf8ea766d1e7bb63939321883ebf91
Jack-of-all-trades-04 <novanagung08@gmail.com>
2026-04-29 09:04:27 +0700

    Update source code iteration 29

--- main.c
+++ main.c
@@ -20,8 +20,8 @@ void vigenereDecrypt(char *ciphertext, char *key) {
 }
 
 int main() {
-    char ciphertext[] = "AoGuOFrPaZdyUNK";
-    char key[] = "sQHVB";
+    char ciphertext[] = "SmwUvEBHXgGnhap";
+    char key[] = "WiRIv";
 
     printf("Encrypted Text : %s\n", ciphertext);
     vigenereDecrypt(ciphertext, key);
